f8ada8f89490ee58351fcb4d5981657538d4c3b3
Gyuwon Yi <gyuwon@live.com>
2022-08-15 21:16:22 +0900

    Sellers.Api 프로젝트를 추가한다

---
+++ ShopPlatform/Sellers/Sellers.Api/Program.cs
@@ -0,0 +1,27 @@
+namespace Sellers;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        IServiceCollection services = builder.Services;
+
+        services.AddControllers();
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+
+        var app = builder.Build();
+
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseAuthorization();
+        app.MapControllers();
+        app.Run();
+    }
+}
