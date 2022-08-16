b57ef9f1a4f146e28aceffc029c88d762c39de60
Gyuwon Yi <gyuwon@live.com>
2022-08-17 00:03:43 +0900

    메시징 도구를 추상화 한다

---
+++ ShopPlatform/Orders/Orders.Api/Messaging/IAsyncObservable.cs
@@ -0,0 +1,6 @@
+﻿namespace Orders.Messaging;
+
+public interface IAsyncObservable<T>
+{
+    IDisposable Subscribe(Func<T, Task> onNext);
+}
---
+++ ShopPlatform/Orders/Orders.Api/Messaging/IBus.cs
@@ -0,0 +1,6 @@
+﻿namespace Orders.Messaging;
+
+public interface IBus<T>
+{
+    Task Send(T message);
+}
