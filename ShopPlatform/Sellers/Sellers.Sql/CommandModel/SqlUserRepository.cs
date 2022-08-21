0ef74e6cb5ec1aabeb7b7614fbbffe92b26d13ca
Gyuwon Yi <gyuwon@live.com>
2022-08-21 23:33:34 +0900

    관계형 데이터베이스 기반 IUserRepository 구현

---
+++ ShopPlatform/Sellers/Sellers.Sql/CommandModel/SqlUserRepository.cs
@@ -0,0 +1,21 @@
+﻿namespace Sellers.CommandModel;
+
+public sealed class SqlUserRepository : IUserRepository
+{
+    private readonly Func<SellersDbContext> contextFactory;
+
+    public SqlUserRepository(Func<SellersDbContext> contextFactory)
+        => this.contextFactory = contextFactory;
+
+    public async Task Add(User user)
+    {
+        using SellersDbContext context = contextFactory.Invoke();
+        context.Users.Add(new UserEntity
+        {
+            Id = user.Id,
+            Username = user.Username,
+            PasswordHash = user.PasswordHash,
+        });
+        await context.SaveChangesAsync();
+    }
+}
---
+++ ShopPlatform/Sellers/Sellers.UnitTests/CommandModel/SqlUserRepository_specs.cs
@@ -0,0 +1,22 @@
+﻿using AutoFixture.Xunit2;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+
+namespace Sellers.CommandModel;
+
+public class SqlUserRepository_specs
+{
+    [Theory, AutoSellersData]
+    public async Task Sut_correctly_creates_new_entity(
+        [Frozen] Func<SellersDbContext> contextFactory,
+        SqlUserRepository sut,
+        User user)
+    {
+        await sut.Add(user);
+
+        using SellersDbContext context = contextFactory.Invoke();
+        UserEntity? actual = await context.Users.SingleOrDefaultAsync(x => x.Id == user.Id);
+        actual.Should().BeEquivalentTo(user);
+    }
+}
